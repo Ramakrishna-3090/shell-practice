e823e41541a29a1b8e4eaed27a2af4c2976bbfb7
Ramakrishna P <ramakrishnareddy3090@gmail.com>
2026-01-29 16:45:08 +0530

    action on 12-loops1

--- 12-loops.sh
+++ 12-loops.sh
@@ -7,7 +7,7 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 mkdir -p $LOGS_FOLDER
-VALIDATE () {
+VALIDATE(){
     if [ $1 -ne 0 ]; then
         echo "$2... FAILURE" | tee -a $LOGS_FILE
         exit 1
