f8a26e91e9d6c85f96364157146525673708967a
Ramakrishna P <ramakrishnareddy3090@gmail.com>
2026-01-29 12:36:47 +0530

    action on installation

---
+++ 08-installation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+echo "Installing nginx"
+dnf install nginx -y
+if [ $? -ne 0 ]; then
+    echo "Installing nginx... FAILURE"
+    exit 1
+else
+    echo "Installing nginx.. SUCCESS"
+fi
+dnf install mysql -y
+if [ $? -ne 0 ]; then
+    echo "Installing mysql.. FAILURE"
+    exit 1
+else
+    echo "Installing mysql.. SUCCESS"
+fi
+dnf install nodejs -y
+if [ $? -ne 0 ]; then
+    echo "Installing nodejs.. FAILURE"
+    exit 1
+else
+    echo "Installing nodejs.. SUCCESS"
+fi
